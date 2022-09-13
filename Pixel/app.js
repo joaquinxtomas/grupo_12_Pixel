bda463501962e27df40d216353cc34ab9aeb8877
Jsalvar124 <jsalvar124@gmail.com>
2022-09-12 20:42:31 -0500

    estructura de carpetas y html index

---
+++ Pixel/app.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const path=require('path')
+
+const app=express()
+
+app.use(express.static(path.resolve(__dirname,'public')))
+
+app.get('/', (req,res) => {
+       res.sendFile(path.resolve(__dirname,'./src/views/index.html'))
+})
+
+app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'))
