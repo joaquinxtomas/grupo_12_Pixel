a6a75182273a4a0ef01860c7affb570ea5d312ac
Jsalvar124 <jsalvar124@gmail.com>
2022-09-16 16:44:07 -0500

    Imagenes, avance header, archivo con paleta de colores

--- Pixel/app.js
+++ Pixel/app.js
@@ -6,7 +6,7 @@ const app=express()
 app.use(express.static(path.resolve(__dirname,'public')))
 
 app.get('/', (req,res) => {
-       res.sendFile(path.resolve(__dirname,'./src/views/index.html'))
+       res.sendFile(path.resolve(__dirname,'./src/views/home.html'))
 })
 
 app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'))
