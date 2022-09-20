5f899048c636ca3db760232109a6f8f8f66e3bcb
Jsalvar124 <jsalvar124@gmail.com>
2022-09-20 10:34:39 -0500

    Se unifica el header del carrito de compras, main al 95%

--- Pixel/app.js
+++ Pixel/app.js
@@ -8,6 +8,7 @@ app.use(express.static(path.resolve(__dirname,'public')))
 app.get('/', (req,res) => {
        res.sendFile(path.resolve(__dirname,'./src/views/index.html'))
 })
+
 app.get('/productCart', (req,res) => {
        res.sendFile(path.resolve(__dirname,'./src/views/productCart.html'))
 })
