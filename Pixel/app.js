0ea9e60226cd926b1abe278a251fc6c9ad9470d4
AdrielAnizan <adrielanizan9@gmail.com>
2022-09-21 12:35:08 -0300

    Mercado Liebre

--- Pixel/app.js
+++ Pixel/app.js
@@ -20,6 +20,10 @@ app.get('/login', (req,res) => {
    });
    app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'));
 
-   app.get("/",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/register.html"))
+   app.get('/register', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'./src/views/register.html'))
 })
+
+app.post('/register', (req,res)=>{
+    res.redirect('/');
+});
