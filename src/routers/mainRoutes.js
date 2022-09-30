0ff54ec3b57f044798397d7194578953be67c281
AdrielAnizan <adrielanizan9@gmail.com>
2022-09-30 17:56:59 -0300

    sprint 3 retrospectiva y daily

--- src/routers/mainRoutes.js
+++ src/routers/mainRoutes.js
@@ -4,7 +4,7 @@ let mainControllers = require('../controllers/mainCrontrollers');
 let loginController = require('../controllers/loginController');
 let registerController = require('../controllers/registerCrontroller');
 let productCartController = require('../controllers/productCartController');
-// let productDetailController = require('../controllers/productDetailController');
+
 
 router.get('/', mainControllers.index);
 
@@ -13,7 +13,5 @@ router.get('/login', loginController.login);
 router.get('/register', registerController.register);
 
 router.get('/productCart', productCartController.productCart);
-
-// router.get('/productDetail', productDetailController.productDetail);
 
 module.exports = router;
--- src/routers/productDetailRoutes.js
+++ src/routers/productDetailRoutes.js
@@ -1,19 +1,7 @@
 const express = require('express');
 const router = express.Router();
-// let mainControllers = require('../controllers/mainCrontrollers');
-// let loginController = require('../controllers/loginController');
-// let registerController = require('../controllers/registerCrontroller');
-// let productCartController = require('../controllers/productCartController');
 
 let productDetailController = require('../controllers/productDetailController');
-
-// router.get('/', mainControllers.index);
-
-// router.get('/login', loginController.login);
-
-// router.get('/register', registerController.register);
-
-// router.get('/productCart', productCartController.productCart);
 
 router.get('/', productDetailController.productDetail);
 
