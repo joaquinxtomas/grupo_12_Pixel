88715bdebec350e8ceaea8dc1083d82945f3c845
joaquinxtomas <jtppalacios@gmail.com>
2022-10-20 16:33:40 -0300

    Agrego validaciones register

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,22 @@
+const {validationResult}=require("express-validator")
+const fs = require ("fs")
+
 const userController = {
     login: (req,res) => { 
         return res.render('login');
     },
     register: (req,res) => {
         return res.render('register');
+    },
+    saveRegister:(req,res)=>{
+        const resultValidation=validationResult(req);
+        if ( resultValidation.errors.length>0){
+            return res.render("register",{
+                errors:resultValidation.mapped(),
+                oldData:req.body
+            })
+        } res.redirect ("/")
+
     },
     productCart: (req,res) => {
         return res.render('productCart');
--- src/routers/userRoutes.js
+++ src/routers/userRoutes.js
@@ -1,14 +1,45 @@
 const express = require('express');
+const path=require("path");
 const router = express.Router();
+const multer = require("multer")
+const {body}=require("express-validator");
 
 
 let userController = require('../controllers/userController');
 
+const storage= multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,path.resolve(__dirname,"../../public/img/avatar"))
+    },
+    filename:function(req,file,cb){
+        cb(null, "AVATAR-IMG" + Date.now()+path.extname(file.originalname))
+    }
+})
 
+const upload = multer ({storage});
+
+const validations = [
+    body('fullName').notEmpty().withMessage("Debes escribir tu nombre completo"),
+    body('username').notEmpty().withMessage("Debes escribir tu nombre de usuario"),
+    body('email')
+    .notEmpty().withMessage("Debes escribir un email").bail()
+    .isEmail().withMessage("Debes escribir un formato de email válido"),
+    body('address').notEmpty().withMessage("Debes escribir tu dirección"),
+    body('password').notEmpty().withMessage("Debes escribir tu contraseña"),
+    body('passwordConfirm').notEmpty().withMessage("Debes confirmar tu contraseña"),
+    body("userImg").custom ((value,{req}) =>{
+        let file=req.file;
+        if (!file){
+            throw new Error("Debes subir una imagen");
+        }
+        return true
+    })
+]
 
 router.get('/login', userController.login);
 
 router.get('/register', userController.register);
+router.post('/register', upload.single("userImg"), validations, userController.saveRegister);
 
 router.get('/productCart', userController.productCart);
 
